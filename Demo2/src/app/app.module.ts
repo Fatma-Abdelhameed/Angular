98381d79e54649e576cb5ee5b41a1f74fb831331
unknown <fatmaabdelhameed1999@gmail.com>
2022-05-23 16:08:56 +0200

    Add core module and navber and footer component

--- Demo2/src/app/app.module.ts
+++ Demo2/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    FooterComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
