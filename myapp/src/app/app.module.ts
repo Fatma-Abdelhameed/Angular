a4392dc2549eff799246d017e0ca610fe1d9aece
unknown <fatmaabdelhameed1999@gmail.com>
2022-05-23 12:19:31 +0200

    Add Header, Footer, Sidebar Component to redtie html page

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { StudentComponent } from './student/student.component';
+import { DepartmentComponent } from './department/department.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent, StudentComponent ],
+    AppComponent, StudentComponent, DepartmentComponent, HeaderComponent, FooterComponent, SidebarComponent ],
   imports: [
     BrowserModule
   ],
